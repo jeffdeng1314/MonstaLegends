57c431298ac7d9e9e8a2b46611b241b18baeee3d
jeffdeng1314 <dengh@oregonstate.edu>
2019-09-06 21:23:32 -0700

    Fixed the seg fault from deserialization

--- HeaderFiles/account.h
+++ HeaderFiles/account.h
@@ -5,5 +5,5 @@
 
 #include "allHeaders.h"
 int account_menu();
-bool user_account(int choice, character c1);
-void character_info(character c1);
+bool user_account(int choice, character *c1);
+void character_info(character *c1);
